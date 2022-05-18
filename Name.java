0da6fa49a8be359345754a7dff509ddde3ff0f25
1user-name-555 <artempohasiy@gmail.com>
2022-05-18 13:45:02 +0300

    first commit

---
+++ Name.java
@@ -0,0 +1,10 @@
+package ff;
+
+public class Name {
+	public static void main(String[] args) {
+		String y = Character.toString( 128_512 );
+		System.out.println( y ) ;	
+		System.out.println("\u2639") ;
+		System.out.println("🚀");
+	}
+}
